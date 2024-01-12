773af0768a9fbcd392c258fe60393267f5bcb8ad
GT <godstimeikoro@gmail.com>
2024-01-12 13:05:11 +0100

    changed the middleware

---
+++ src/app/see-events/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeeEvents = () => {
+  return <div>SeeEvents</div>;
+};
+
+export default SeeEvents;
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/see-events"],
+  debug: process.env.NODE_ENV == "production" ? false : true,
 });
 
 export const config = {
