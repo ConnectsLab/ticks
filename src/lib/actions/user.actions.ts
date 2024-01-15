5091e0a358ad7d70b70ab7de192a1634e04c5764
GT <godstimeikoro@gmail.com>
2024-01-15 16:15:00 +0100

    added new User

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -13,6 +13,8 @@ export const createUser = async (user: User) => {
 
     const newUser = await User.create(user);
 
+    console.log(newUser);
+
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     console.log(error);
